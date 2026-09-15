1c78a80740ef39c29d6d191e40989488058c72e3
xiaozyu <84734377+xiaozyu@users.noreply.github.com>
2026-09-16 02:44:09 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,3 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => clients.claim());
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
